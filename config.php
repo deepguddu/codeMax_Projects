0838ba86ed71b7e024255a8024a7ba08fa0ea469
deepguddu <44464405+deepguddu@users.noreply.github.com>
2018-10-25 16:56:25 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php //session_start();
+
+include_once('classes/get_db_connect.class');
+include_once('classes/comman_function.class');
+include_once('classes/password_endoce_decode.class');
+define('SITE_URL','http://localhost/codeMax');
+
+?>
